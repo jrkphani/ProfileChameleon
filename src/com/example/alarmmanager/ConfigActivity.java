64f39c3c689f084fe5b683f59f2686a8afb1c6b0
dc-mani <manikandan@digitalchakra.in>
2013-08-19 16:10:46 +0530

    account base event query and all button added

--- src/com/example/alarmmanager/ConfigActivity.java
+++ src/com/example/alarmmanager/ConfigActivity.java
@@ -4,14 +4,17 @@ import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Set;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.app.Activity;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
+import android.database.Cursor;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
@@ -33,6 +36,12 @@ public class ConfigActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_config);
+		
+		ArrayList<String> Accounts_list=new ArrayList<String>();
+		String to_push="";
+		String acc_name="";
+		int i=0;
+		int j=0;
 		SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(this); 
 		String modeToSet = settings.getString("configMode", "VIBRATE");
 		/*Log.d("cuurent mode","=======================" );
@@ -55,74 +64,94 @@ public class ConfigActivity extends Activity {
         default:
         	break;
 		}
-		//create radio button for account list
-		
-		/*RadioGroup account_rgroup=(RadioGroup)findViewById(R.id.radiogroup_account_list);
-		RadioButton rb;
-		//get array size of stored account list
-		int account_list_size = settings.getInt("accounts_list_size", 0);
-		if(account_list_size>0)
-		{
-			String acc_name="";
-			for(int i=0; i<account_list_size; i++)
-			{
-				//get account name
-				acc_name = settings.getString("acc"+i, null);
-				if(acc_name != null)
-				{
-					rb=new RadioButton(this);
-				       rb.setText(acc_name);
-				       rb.setId(i);
-				       account_rgroup.addView(rb);
-				}
-				           
-			}
-		}*/
 		
 		
 		
 		//create checkbox for account list
 		CheckBox rb ;
-		
+		int account_selected_all = settings.getInt("accounts_selected_all", 0);
 		LinearLayout parentLinear = (LinearLayout)findViewById(R.id.parent_linear);
 		//get array size of stored account list
-				int account_list_size = settings.getInt("accounts_list_size", 0);
-				if(account_list_size>0)
-				{
-					String acc_name="";
-					for(int i=0; i<account_list_size; i++)
-					{
-						//get account name
-						acc_name = settings.getString("acc"+i, null);
-						if(acc_name != null)
-						{
-							rb = new CheckBox(this);
-						       rb.setText(acc_name);
-						       rb.setId(i);
-						       rb.setOnClickListener(new OnClickListener() {
-								
-									@Override
-									public void onClick(View v) {
-										// TODO Auto-generated method stub
-										String acc_txt = ((CheckBox) v).getText().toString();
-										if (((CheckBox) v).isChecked())
-										{
-											checkedAccounts.put(acc_txt,acc_txt);
-											Toast.makeText(getBaseContext(),"added",Toast.LENGTH_SHORT).show();
-										}
-										else
-										{
-											checkedAccounts.remove(acc_txt);
-											Toast.makeText(getBaseContext(),"removed",Toast.LENGTH_SHORT).show();
-										}
-									}
-						       });
-						       parentLinear.addView(rb);
-						       //this.setContentView(rb);
-						}
-						           
-					}
-				}
+		Uri calendaruri = Uri.parse("content://com.android.calendar/events");
+		Cursor mCursor = getContentResolver().query(calendaruri, null, null, null, null);
+		if(mCursor.getCount()>0)
+		 {
+			mCursor.moveToFirst();
+			while(mCursor.moveToNext())
+			 {
+				if(!to_push.equals(mCursor.getString(mCursor.getColumnIndex("account_name"))))
+				 {
+					 to_push =mCursor.getString(mCursor.getColumnIndex("account_name"));
+					//check if account exist in array
+					 if(!Accounts_list.contains(to_push))
+					 {
+						 Accounts_list.add(to_push);
+					 }
+				 }
+			 }
+			if(Accounts_list.size() >0)
+			 {
+				CheckBox all_acount = (CheckBox)findViewById(R.id.all_acount);
+				all_acount.setVisibility(1);
+				int accounts_selected_size = settings.getInt("accounts_selected_size", 0);
+				 for(i=0;i<Accounts_list.size();i++)
+				 {
+					 //settingsEditor.putString("acc"+i,Accounts_list.get(i));
+					 rb = new CheckBox(this);
+				       rb.setText(Accounts_list.get(i));
+				       rb.setId(i);
+				       if(account_selected_all<=0)
+				       {
+				    	   if(accounts_selected_size>0)
+							{
+					    	   for(j=0; j<accounts_selected_size; j++)
+								{
+					    		   acc_name = settings.getString("acc_selected"+j, null);
+					    		   Log.d("selected acc",acc_name);
+					    		   Log.d("selected aaacc",Accounts_list.get(i));
+					    		   if(Accounts_list.get(i).equals(acc_name))
+					    		   {
+					    			   checkedAccounts.put(acc_name,acc_name);
+					    			   rb.setChecked(true);
+								      // break;
+					    		   }
+								}
+							}
+				       }
+				       else
+				       {
+				    	   all_acount.setChecked(true);
+				    	   rb.setChecked(true);
+				       }
+				       
+				       rb.setOnClickListener(new OnClickListener() {
+						
+							@Override
+							public void onClick(View v) {
+								// TODO Auto-generated method stub
+								String acc_txt = ((CheckBox) v).getText().toString();
+								if (((CheckBox) v).isChecked())
+								{
+									checkedAccounts.put(acc_txt,acc_txt);
+									Toast.makeText(getBaseContext(),"added",Toast.LENGTH_SHORT).show();
+								}
+								else
+								{
+									checkedAccounts.remove(acc_txt);
+									Toast.makeText(getBaseContext(),"removed",Toast.LENGTH_SHORT).show();
+								}
+							}
+				       });
+				       parentLinear.addView(rb);
+				 }
+			 }
+			
+		 }
+		else
+		{
+			TextView Account_list_title = (TextView)findViewById(R.id.Account_list_title);
+			Account_list_title.setText("No accounts with events");
+		}
 				/*
 				//texting to get save selected account list
 				//create checkbox for account list
@@ -147,22 +176,36 @@ public class ConfigActivity extends Activity {
 	{
 		RadioGroup g = (RadioGroup) findViewById(R.id.radiogroup_mode);
 		int selected = g.getCheckedRadioButtonId();
+		int i=0;
 		RadioButton b = (RadioButton) findViewById(selected);
 		String selectedMode = (String) b.getText();
 		SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(this); 
 		SharedPreferences.Editor settingsEditor = settings.edit();
 		settingsEditor.putString("configMode", selectedMode);
+		settingsEditor.remove("%acc_selected");
+		settingsEditor.remove("accounts_selected_size");
+		settingsEditor.remove("accounts_selected_all");
 		//Store the account list in to SharedPreferences
-		 if(checkedAccounts.size() >0)
-		 {
-			 Object[] checkedAccountslist = checkedAccounts.keySet().toArray();
-			 for(int i=0;i<checkedAccounts.size();i++)
+		CheckBox all_acount = (CheckBox)findViewById(R.id.all_acount);
+		if(all_acount.isChecked())
+		{
+			settingsEditor.putInt("accounts_selected_all",1);
+			//Log.d("all account","all account");
+		}
+		else
+		{
+			 if(checkedAccounts.size() >0)
 			 {
-				settingsEditor.putString("acc_selected"+i,checkedAccountslist[i].toString());
-				//Log.d("------------",checkedAccountslist[i].toString());
+				 Object[] checkedAccountslist = checkedAccounts.keySet().toArray();
+				 for(i=0;i<checkedAccounts.size();i++)
+				 {
+						settingsEditor.putString("acc_selected"+i,checkedAccountslist[i].toString());
+						Log.d("------------",checkedAccountslist[i].toString()); 
+				 }
+				 //to remove if any account is stored previous
+				 settingsEditor.putInt("accounts_selected_size",checkedAccounts.size());
 			 }
-			 settingsEditor.putInt("accounts_selected_size",checkedAccounts.size());
-		 }
+		}
 		settingsEditor.commit();
 		//Toast.makeText(getBaseContext(),selectedMode + " Saved",Toast.LENGTH_SHORT).show();
 		Toast.makeText(getBaseContext(),"Saved",Toast.LENGTH_SHORT).show();
--- src/com/example/alarmmanager/Service_class.java
+++ src/com/example/alarmmanager/Service_class.java
@@ -6,6 +6,7 @@ import java.util.Date;
 import java.util.HashSet;
 import java.util.Set;
 
+import android.annotation.TargetApi;
 import android.app.Notification;
 import android.app.Notification.Builder;
 import android.app.NotificationManager;
@@ -18,10 +19,16 @@ import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.media.AudioManager;
 import android.net.Uri;
+import android.os.Build;
 import android.os.IBinder;
 import android.preference.PreferenceManager;
+import android.provider.CalendarContract.Calendars;
+import android.provider.CalendarContract.Events;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.CheckBox;
 import android.widget.Toast;
 
 
@@ -35,139 +42,93 @@ public class Service_class extends Service {
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         //Toast.makeText(this, "Service Started", Toast.LENGTH_LONG).show();
+    	SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(this);
+		SharedPreferences.Editor settingsEditor = settings.edit();
     	AudioManager audio = (AudioManager)getSystemService(Context.AUDIO_SERVICE);
         String flag = "need genral profile";
         int general = 0;
         int vibrate = 0;
-        final int mId = 1014;
+        int i=0;
 		String str_disp = "";
 		String mode_changed = "no";
 		String to_push="";
 		ArrayList<String> Accounts_list=new ArrayList<String>();
 		//String prev_mode_changed = "no";
-		final NotificationCompat.Builder mBuilder =
-		        new NotificationCompat.Builder(this)
-				.setContentTitle("My notification")
-		        .setSmallIcon(R.drawable.notification_icon);
+		
 		Uri calendaruri = Uri.parse("content://com.android.calendar/events");
-		Cursor mCursor = getContentResolver().query(calendaruri, null, null, null, null);
-		if(mCursor.getCount()>0)
-		 {
-			SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(this);
-			SharedPreferences.Editor settingsEditor = settings.edit();
-			//get prefer mode
-			String modeToSet = settings.getString("configMode", "VIBRATE");
-			
-			
-			mCursor.moveToFirst();
-			/////////
-			/*
-			//to get list fo index 
-            for (int i = 0; i < mCursor.getCount(); i++) {
-                 // retrieve the calendar names and ids
-                 Log.d("==***********====",mCursor.getColumnName(i));
-             }
-            /////////
-             */
-			
-			 while(mCursor.moveToNext())
-			 {
-				 long currentTime = new Date().getTime();
-				 
-				 /*str_disp = str_disp+"Title : "+mCursor.getString(mCursor.getColumnIndex("title"))
-						 +"\nStart -Date : "+ mCursor.getLong(mCursor.getColumnIndex("dtstart"))
-						 +"\nEnd -- Date : "+ mCursor.getLong(mCursor.getColumnIndex("dtend"))
-						 +"\ncurrentTime: "+currentTime+"\n __________________ \n\n";*/
-				 //store the account in to array list
-				 if(!to_push.equals(mCursor.getString(mCursor.getColumnIndex("account_name"))))
-				 {
-					 to_push =mCursor.getString(mCursor.getColumnIndex("account_name"));
-					//check if account exist in array
-					 if(!Accounts_list.contains(to_push))
-					 {
-						 Accounts_list.add(to_push);
-					 }
-				 }
-				 
-					 if((currentTime >= mCursor.getLong(mCursor.getColumnIndex("dtstart")) ) && (currentTime <= mCursor.getLong(mCursor.getColumnIndex("dtend"))))
-					 {
-							 //audio.setRingerMode(audio.RINGER_MODE_VIBRATE);
-							 vibrate=1;
-						 //flag ="need silent profile";
-						 //audio.setRingerMode(audio.RINGER_MODE_VIBRATE);
-						 //Toast.makeText(getApplicationContext(), mCursor.getString(mCursor.getColumnIndex("account_name")), Toast.LENGTH_SHORT).show();
-					 }
-					/* else
-					 {
-						 if(vibrate !=1)
-						 {
-							 general=1;
-						 //audio.setRingerMode(audio.RINGER_MODE_NORMAL);
-						// Toast.makeText(getApplicationContext(), "n", Toast.LENGTH_SHORT).show();
+		Cursor mCursor;
+		//String selection = "((" + Calendars.ACCOUNT_NAME + " = ?) AND ("+ Calendars.ACCOUNT_NAME + " = ?) AND ("+ Calendars.ACCOUNT_NAME + " = ?))";
+		//String[] selectionArgs = new String[] {"manimani1014@gmail.com","manikandan@digitalchakra.in","test@gmail.com"}; 
+		final String[] EVENT_PROJECTION = new String[] {
+				    Calendars._ID,
+				    Calendars.ACCOUNT_NAME,
+				    Calendars.CALENDAR_DISPLAY_NAME,
+				    Calendars.OWNER_ACCOUNT,
+				    Events.DTEND,
+				    Events.DTSTART
+				};
+		int account_selected_size = settings.getInt("accounts_selected_size", 0);
+		int account_selected_all = settings.getInt("accounts_selected_all", 0);
+		if(account_selected_all == 0)
+		{
+			if(account_selected_size>0)
+			{
+				String acc_name="";
+				for(i=0; i<account_selected_size; i++)
+				{
+					//get account name
+					acc_name = settings.getString("acc_selected"+i, null);
+					if(acc_name != null && vibrate == 0)
+					{
+						String selection = "((" + Calendars.ACCOUNT_NAME + " = ?))";
+						String[] selectionArgs = new String[] {acc_name};
+						mCursor = getContentResolver().query(calendaruri, EVENT_PROJECTION, selection, selectionArgs, null);
+						if(mCursor.getCount()>0)
+						 {							
+							vibrate = Check_event(mCursor);
+							 
+							 if(vibrate==1)
+							 {
+								//get prefer mode
+								String modeToSet = settings.getString("configMode", "VIBRATE");
+								mode_changed = Change_profile(modeToSet);
+							 }
+							 else
+							 {
+								 if(audio.getRingerMode() !=2)
+								 {
+									 //set to normal mode when there is no event
+									 audio.setRingerMode(audio.RINGER_MODE_NORMAL);
+									 mode_changed = "NORMAL";
+								 }
+								 
+							 }
+							 
+							 //notification
+							if(!mode_changed.equals("no"))
+							 {
+								notification_top(mode_changed);
+							 }
 						 }
-					 }*/
-
-			 }
-			 
-			 //Store the account list in to SharedPreferences
-			 if(Accounts_list.size() >0)
-			 {
-				 for(int i=0;i<Accounts_list.size();i++)
-				 {
-					 settingsEditor.putString("acc"+i,Accounts_list.get(i));
-				 }
-				 settingsEditor.putInt("accounts_list_size",Accounts_list.size());
-				 settingsEditor.commit();
-			 }
+						
+					}
+					           
+				}
+			}
+    	}
+    else
+    {
+    	//Log.d("all account","all account ===");
+    	mCursor = getContentResolver().query(calendaruri, null, null, null, null);
+    	if(mCursor.getCount()>0)
+		 {							
+			vibrate = Check_event(mCursor);
 			 
 			 if(vibrate==1)
 			 {
-				//change profile mode to user preferred mode when there is an event
-				 
-				 //refer the comparing string ConfigActivity
-				 if(modeToSet.equals("SILENT"))
-				 {
-					 //change mode if mode is not in 'SILENT' 
-					// Toast.makeText(getApplicationContext(), "SILET CON", Toast.LENGTH_SHORT).show();
-					 if(audio.getRingerMode() !=0)
-					 {
-						 audio.setRingerMode(audio.RINGER_MODE_SILENT);
-						 mode_changed = "SILENT";
-					 }
-				 }
-				 else if(modeToSet.equals("VIBRATE"))
-				 {
-					//change mode if mode is not in 'VIBRATE'
-					// Toast.makeText(getApplicationContext(), "VIBRATE CON", Toast.LENGTH_SHORT).show();
-					 if(audio.getRingerMode() !=1)
-					 {
-						 audio.setRingerMode(audio.RINGER_MODE_VIBRATE);
-						 mode_changed = "VIBRATE";
-					 }
-				 }
-				 else if(modeToSet.equals("NORMAL"))
-				 {
-					//change mode if mode is not in 'NORMAL'
-					// Toast.makeText(getApplicationContext(), "NORMAL CON", Toast.LENGTH_SHORT).show();
-					 if(audio.getRingerMode() !=2)
-					 {
-						 audio.setRingerMode(audio.RINGER_MODE_NORMAL);
-						 mode_changed = "NORMAL";
-					 }
-				 }
-				 else
-				 {
-					//change profile mode default case to vibrate if any error occurs
-					 if(audio.getRingerMode() !=1)
-					 {
-						 audio.setRingerMode(audio.RINGER_MODE_VIBRATE);
-						 mode_changed = "VIBRATE";
-					 }
-				 }
-				 
-				 
-				 
-				 
+				//get prefer mode
+				String modeToSet = settings.getString("configMode", "VIBRATE");
+				mode_changed = Change_profile(modeToSet);
 			 }
 			 else
 			 {
@@ -175,56 +136,19 @@ public class Service_class extends Service {
 				 {
 					 //set to normal mode when there is no event
 					 audio.setRingerMode(audio.RINGER_MODE_NORMAL);
-					 //prev_mode_changed = "yes";
 					 mode_changed = "NORMAL";
 				 }
 				 
 			 }
-			 //Log.d("stored mode", modeToSet);
-			// Toast.makeText(getApplicationContext(), "stored mode "+modeToSet, Toast.LENGTH_SHORT).show();
 			 
 			 //notification
-			 //if((!mode_changed.equals("no")) || (!prev_mode_changed.equals("no")))
 			if(!mode_changed.equals("no"))
 			 {
-				// Toast.makeText(getApplicationContext(), "Changing mode", Toast.LENGTH_SHORT).show();
-					
-				 mBuilder.setContentText("Profile changed to "+mode_changed).setAutoCancel(true);
-
-					// Creates an explicit intent for an Activity in your app
-					Intent resultIntent = new Intent(this, MainActivity.class);
-
-					// The stack builder object will contain an artificial back stack for the
-					// started Activity.
-					// This ensures that navigating backward from the Activity leads out of
-					// your application to the Home screen.
-					TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
-					// Adds the back stack for the Intent (but not the Intent itself)
-					stackBuilder.addParentStack(MainActivity.class);
-					// Adds the Intent that starts the Activity to the top of the stack
-					stackBuilder.addNextIntent(resultIntent);
-					PendingIntent resultPendingIntent =
-					        stackBuilder.getPendingIntent(
-					            0,
-					            PendingIntent.FLAG_UPDATE_CURRENT
-					        );
-					mBuilder.setContentIntent(resultPendingIntent);
-					NotificationManager mNotificationManager =
-						    (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-						// mId allows you to update the notification later on.
-						mNotificationManager.notify(mId, mBuilder.build());
-				 
+				notification_top(mode_changed);
 			 }
 		 }
-		 else
-		 {
-			// Toast.makeText(getApplicationContext(), "No Events", Toast.LENGTH_SHORT).show();
-		 }
-
-		//Toast.makeText(getApplicationContext(), "Service Runing", Toast.LENGTH_SHORT).show();
-		//System.out.println("wegggggg");
-		
-		//System.out.println(getString(audio.getRingerMode()));
+    	
+    }
         return START_STICKY;
     }
 
@@ -239,6 +163,103 @@ public class Service_class extends Service {
     @Override
     public void onCreate() {
         super.onCreate();
+    }
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+	private void notification_top(String mode_changed)
+    {
+    	//this md will be used to modify the notification
+        final int mId = 1014;
+    	final NotificationCompat.Builder mBuilder =
+		        new NotificationCompat.Builder(this)
+				.setContentTitle("My notification")
+		        .setSmallIcon(R.drawable.notification_icon);
+    	// Toast.makeText(getApplicationContext(), "Changing mode", Toast.LENGTH_SHORT).show();
+		
+		 mBuilder.setContentText("Profile changed to "+mode_changed).setAutoCancel(true);
+
+			// Creates an explicit intent for an Activity in your app
+			Intent resultIntent = new Intent(this, MainActivity.class);
+
+			// The stack builder object will contain an artificial back stack for the
+			// started Activity.
+			// This ensures that navigating backward from the Activity leads out of
+			// your application to the Home screen.
+			TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+			// Adds the back stack for the Intent (but not the Intent itself)
+			stackBuilder.addParentStack(MainActivity.class);
+			// Adds the Intent that starts the Activity to the top of the stack
+			stackBuilder.addNextIntent(resultIntent);
+			PendingIntent resultPendingIntent =
+			        stackBuilder.getPendingIntent(
+			            0,
+			            PendingIntent.FLAG_UPDATE_CURRENT
+			        );
+			mBuilder.setContentIntent(resultPendingIntent);
+			NotificationManager mNotificationManager =
+				    (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+				// mId allows you to update the notification later on.
+				mNotificationManager.notify(mId, mBuilder.build());
+    }
+    
+    private int Check_event(Cursor mCursor)
+    {
+    	mCursor.moveToFirst();
+    	 int vibrate=0;
+		 while(mCursor.moveToNext())
+		 {
+			 long currentTime = new Date().getTime();
+				 if((currentTime >= mCursor.getLong(mCursor.getColumnIndex("dtstart")) ) && (currentTime <= mCursor.getLong(mCursor.getColumnIndex("dtend"))))
+				 {
+						 vibrate=1;
+				 }
+
+		 }
+		 return  vibrate;
+    }
+    private String Change_profile(String modeToSet)
+    {
+    	AudioManager audio = (AudioManager)getSystemService(Context.AUDIO_SERVICE);
+    	//change profile mode to user preferred mode when there is an event
+		 
+		 //refer the comparing string ConfigActivity
+		 if(modeToSet.equals("SILENT"))
+		 {
+			 //change mode if mode is not in 'SILENT' 
+			// Toast.makeText(getApplicationContext(), "SILET CON", Toast.LENGTH_SHORT).show();
+			 if(audio.getRingerMode() !=0)
+			 {
+				 audio.setRingerMode(audio.RINGER_MODE_SILENT);
+			 }
+		 }
+		 else if(modeToSet.equals("VIBRATE"))
+		 {
+			//change mode if mode is not in 'VIBRATE'
+			// Toast.makeText(getApplicationContext(), "VIBRATE CON", Toast.LENGTH_SHORT).show();
+			 if(audio.getRingerMode() !=1)
+			 {
+				 audio.setRingerMode(audio.RINGER_MODE_VIBRATE);
+			 }
+		 }
+		 else if(modeToSet.equals("NORMAL"))
+		 {
+			//change mode if mode is not in 'NORMAL'
+			// Toast.makeText(getApplicationContext(), "NORMAL CON", Toast.LENGTH_SHORT).show();
+			 if(audio.getRingerMode() !=2)
+			 {
+				 audio.setRingerMode(audio.RINGER_MODE_NORMAL);
+			 }
+		 }
+		 else
+		 {
+			//change profile mode default case to vibrate if any error occurs
+			 if(audio.getRingerMode() !=1)
+			 {
+				 audio.setRingerMode(audio.RINGER_MODE_VIBRATE);
+			 }
+		 }
+		return modeToSet;
+		 
+		 
     }
 
 }
