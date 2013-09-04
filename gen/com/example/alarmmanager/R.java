ddf093e4a10350ce9442253b92c6753b00707ff7
dc-mani <manikandan@digitalchakra.in>
2013-09-04 13:39:36 +0530

    always dispaly buttons at bottom

--- gen/com/example/alarmmanager/R.java
+++ gen/com/example/alarmmanager/R.java
@@ -26,17 +26,18 @@ public final class R {
     }
     public static final class id {
         public static final int Account_list_title=0x7f080005;
-        public static final int action_settings=0x7f08000c;
+        public static final int action_settings=0x7f08000d;
         public static final int all_acount=0x7f080006;
-        public static final int configBtn=0x7f08000b;
+        public static final int back=0x7f080009;
+        public static final int configBtn=0x7f08000c;
         public static final int parent_linear=0x7f080007;
         public static final int radio_normal=0x7f080003;
         public static final int radio_silent=0x7f080004;
         public static final int radio_vibrate=0x7f080002;
         public static final int radiogroup_mode=0x7f080001;
         public static final int saveBtn=0x7f080008;
-        public static final int startserviceBtn=0x7f080009;
-        public static final int stopserviceBtn=0x7f08000a;
+        public static final int startserviceBtn=0x7f08000a;
+        public static final int stopserviceBtn=0x7f08000b;
         public static final int textView1=0x7f080000;
     }
     public static final class layout {
