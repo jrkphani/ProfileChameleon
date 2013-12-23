f8c53fbeda23edea86c98e92802ef83e878965ae
Manikandan R <manikandan@digitalchakra.in>
2013-12-23 12:57:40 +0530

    text chages

--- gen/com/example/alarmmanager/R.java
+++ gen/com/example/alarmmanager/R.java
@@ -72,7 +72,8 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
-        public static final int description_config=0x7f050005;
+        public static final int description_config1=0x7f050005;
+        public static final int description_config2=0x7f050006;
         public static final int description_main=0x7f050004;
         public static final int hello_world=0x7f050002;
         public static final int title_activity_config=0x7f050003;
