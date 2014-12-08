1714d6e560a371f2324edac9bee14163895427b0
ManiKandan Ram <manikandan@digitalchakra.in>
2014-12-08 13:09:26 +0530

    Rate me option added as popup and in optional menu

--- gen/in/digitalchakra/profilechameleon/R.java
+++ gen/in/digitalchakra/profilechameleon/R.java
@@ -63,18 +63,24 @@ public final class R {
         public static final int main=0x7f070002;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
+        public static final int action_settings=0x7f050002;
         public static final int app_name=0x7f050000;
-        public static final int dc=0x7f05000a;
-        public static final int description_about=0x7f050009;
-        public static final int description_config1=0x7f050006;
-        public static final int description_config2=0x7f050007;
-        public static final int description_main1=0x7f050004;
-        public static final int description_main2=0x7f050005;
-        public static final int hello_world=0x7f050002;
-        public static final int selectHrs=0x7f05000b;
-        public static final int title_activity_about=0x7f050008;
-        public static final int title_activity_config=0x7f050003;
+        public static final int dc=0x7f05000b;
+        public static final int description_about=0x7f05000a;
+        public static final int description_config1=0x7f050007;
+        public static final int description_config2=0x7f050008;
+        public static final int description_main1=0x7f050005;
+        public static final int description_main2=0x7f050006;
+        public static final int hello_world=0x7f050003;
+        public static final int rate_me=0x7f050001;
+        public static final int rta_dialog_cancel=0x7f050010;
+        public static final int rta_dialog_message=0x7f05000e;
+        public static final int rta_dialog_no=0x7f050011;
+        public static final int rta_dialog_ok=0x7f05000f;
+        public static final int rta_dialog_title=0x7f05000d;
+        public static final int selectHrs=0x7f05000c;
+        public static final int title_activity_about=0x7f050009;
+        public static final int title_activity_config=0x7f050004;
     }
     public static final class style {
         /** 
